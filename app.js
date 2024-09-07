2be5a35b3b23059d1c807d206a81a589301ca1f9
VitorLuizSimette <vitorluiz.estephanes@gmail.com>
2024-09-07 16:13:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById
+    ("resultados-pesquisa");
+    
+let campoPesquisa = document.getElementById
+("campo-pesquisa").value
+
+// se campoPesquisa for um strig sem nada
+if (!campoPesquisa) {
+  section.innerHTML = "<p>Nenhum brawler foi encontrado. Digite o nome do brawler ou alguma coisa relacionada diretamente a ele.</p>"
+  return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+// Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  let estatisticas = "";
+  let modosDeJogo = "";
+  let dicasEstrategias = "";
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      // se titulo includes campoPesquisa
+            if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) || estatisticas.includes(campoPesquisa) || modosDeJogo.includes(campoPesquisa) || dicasEstrategias.includes(campoPesquisa)) {
+        // cria um novo elemento
+        resultados += `
+<div class="item-resultado">
+  <h2>
+    <a href="#" target="_blank">${dado.titulo}</a>
+  </h2>
+  <img src = "${dado.imagem}" alt = "${dado.titulo}" width = "100" height = "100">
+  <p class="descricao-meta">${dado.descricao}</p>
+  <p class="descricao-meta">${dado.estatisticas}</p>
+  <p class="descricao-meta">${dado.modosDeJogo}</p>
+  <p class="descricao-meta">${dado.dicasEstrategias}</p>
+  <a href=${dado.link} target="_blank">Mais informações</a>
+
+  </div>
+`;
+            }
+      }
+  
+if (!resultados) {
+  resultados = "<p><p>Nenhum brawler foi encontrado. Digite o nome do brawler ou alguma coisa relacionada diretamente a ele.</p>"
+}
+
+
+    // Atribui o HTML construído à seção de resultados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+        titulo : "Colt",
+        imagem: "Imagem Colt.png",
+        descricao : "Colt é um dos personagens mais populares de Brawl Stars, conhecido por sua velocidade e habilidade com as pistolas. Com um visual clássico de pistoleiro, ele é um dos brawlers mais ágeis do jogo, capaz de se mover rapidamente pelo mapa e desferir tiros precisos em seus inimigos. Apesar de sua fragilidade, Colt compensa com seu dano por segundo elevado e sua capacidade de eliminar inimigos à distância. Sua habilidade especial, Super, permite que ele dispare uma rajada de tiros ainda mais longa, tornando-o uma ameaça formidável em combate.",
+        link : "https://pt.wikipedia.org/wiki/Brawl_Stars#", 
+            tags : "Rápido Rapido rapido rápido RÁPIDO RAPIDO letal Letal LETAL Versátil versatil Versatil VERSÁTIL VERSATIL",
+            estatisticas : "Estatísticas: Vida = 5600; Dano = 720; Velocidade = 820.",
+            modosDeJogo : "Melhores modos de jogo: Roubo, Zona Estratégica e Combate Duplo.",
+            dicasEstrategias : "Dicas e Estratégias = Tente manter sua distância. Este Brawler é especializado em combate de longo alcance e pode ultrapassar Brawlers como a Shelly, El Primo e Bull facilmente. Ao lutar com um Brawler com alcance parecido, como o Ricochet, encontre alguns arbustos para se esconder. O elemento surpresa pode lhe dar vantagem. Ele tem um tempo de recarga muito rápido. Isso pode lhe dar uma vantagem sobre Brawlers, como Brock, cujo tempo de recarga é relativamente lento. O seu Super ataque “Trem-bala”, pode ser usado para devastar os inimigos diretamente, ou pode ser usado para destruir a cobertura do inimigo para que o resto da equipe possa entrar e terminar o trabalho. Também é possível usa-lo para controle de área por um curto período.As Botas Estilosas do Colt permitem que ele se mova mais rápido, tornando mais fácil perseguir ou escapar para se proteger. Esse acessório também é excelente para desviar de projéteis de outros Brawlers de longo alcance. Em Eventos de equipe, é melhor combinar Colt com Brawlers com habilidades de atordoamento, puxão e lentidão para facilitar o acerto de todas as suas balas."
+    },
+     {
+        titulo: "Piper",
+        imagem: "Imagem Piper2.jpg",
+        descricao: "Piper é uma brawler de longo alcance conhecida por sua precisão e dano. Com seu rifle de precisão, ela pode eliminar inimigos de longe. Além disso, sua Super permite que ela pule deixando 4 bombas no lugar onde estava antes de pular, tornando-a uma excelente opção para eliminar inimigos prioritários.",
+        link: "https://pt.wikipedia.org/wiki/Brawl Stars#",
+        tags: "Precisa precisa PRECISA ágil Ágil Agil agil ÁGIL AGIL Distante distante DISTANTE",
+        estatisticas: "Estatísticas: Vida = 4.600; Dano (alcance mínimo) = 883, Dano (alcance máximo) = 3.400; Velocidade = 720.",
+        modosDeJogo: "Melhores modos de jogo: Combate Solitário, Combate Duplo e Caça-Estrelas.",
+        dicasEstrategias: "Dicas e Estratégias: Certifique-se de mantê-la o mais longe possível dos adversários, não só para evitar o fogo inimigo, mas também para maximizar o dano do seu ataque. Ela pode ser oprimida e derrotada facilmente se estiver muito exposta. Ela é mais eficaz quando ela tem cobertura de outros brawlers da sua equipe. Se os adversários chegarem perto demais, pode ser uma boa ideia usar o Super dela para escapar, caso o super estiver carregado. Tente prever o movimento do seu alvo ao atirar. Os tiros da Piper são pequenos, então você precisará mirar à frente de seus alvos se eles estiverem se movendo para que eles não saiam para fora do caminho do tiro. Esse super também pode ser usado para modificar o mapa a seu favor, destruindo muros que seus oponentes estariam se escondendo. Carregue o Super de Piper como uma prioridade, pois há Brawlers que podem avançar diretamente nela de forma muito rápida, como Darryl , Bull e Mortis."
+      },
+      { titulo: "Dynamike",
+        imagem: "Imagem Dynamike.png",
+      descricao: "Dynamike é um brawler especialista em explosivos. Seus ataques lançam várias dinamites que causam dano em área, sendo ideal para controlar o mapa e eliminar grupos de inimigos. Sua Super permite que ele lance uma dinamite grande que causa dano e quebra paredes, tornando-o uma força a ser considerada em confrontos. No entanto, Dynamike é um brawler frágil, por isso sempre precisa estar longe de seus inimigos.",
+      link: "https://pt.wikipedia.org/wiki.org/Brawl Stars#",
+    tags: "Explosivo explosivo EXPLOSIVO imprevisível impresivisel Imprevisível Imprevisivel IMPREVISÍVEL IMPREVISIVEL demolidor DEMOLIDOR Demolidor",
+    estatisticas: "Estatísticas: Vida = 5.600; Dano = 1.600; Velocidade = 770.",
+    modosDeJogo: "Melhores modos de jogo: Pique-Gema, Combate Solitário, Combate Duplo e Caça-Estrelas.",
+    dicasEstrategias: "Dicas e Estratégias: As dinamites de Dynamike podem causar algum dano massivo se jogadas corretamente. Uma vez que a dinamite leva um pouco de tempo para percorrer o espaço e explodir, em vez de tentar jogá-la diretamente no inimigo, tente jogá-la onde é provável que ele esteja quando a dinamite explodir. Escondendo-se atrás de um arbusto com uma parede na frente dele e jogando a dinamite em direção aos brawlers adversário de lá pode ser eficaz, já que será difícil para a maioria dos outros brawlers chegar até você. Ao jogar o modo Heist, jogue o seu “Big Barril de explosão” na parede em frente ao cofre. Vai explodir a parede e sua equipe não terá que se preocupar em dar a volta para atirar no cofre. Quando estiver sendo perseguido por um inimigo a curta distância, tente jogar dinamite um pouco na sua frente. Isso faz com que seu oponente pare de perseguir ou receba dano do seu ataque. Infelizmente Dyna é combatido com eficiência por brawlers de alcance curto, mas você pode tentar contornar isso jogando suas dinamites um pouco à sua frente. Isso evita que o inimigo perseguidor o persiga ou causa dano ao inimigo. Se eles estiverem entre 3 e 5 ladrilhos de distância, tente jogar um ladrilho na frente de sua posição atual com resultados semelhantes. Tente jogar seu Super para causar dano a pelo menos dois inimigos ao mesmo tempo, pois ele causa dano suficiente para carregar outro Super logo em seguida, permitindo que você encadeie Supers e cause uma infinidade de danos. Apesar de ser um pouco difícil… é possível saltar sobre os ataques inimigos com o Poder de Estrela “Salto Explosivo” do Dynamike. Isso é difícil de fazer, e não se encaixa em todas as jogadas, mas pode ajudar em situações difíceis quando sob fogo pesado."
+    }, 
+      {
+        titulo: "Chester",
+        imagem: "Imagem Chester.png",
+        descricao: "Chester é um brawler lendário e destruidor, conhecido por suas brincadeiras e ataques imprevisíveis. Ele lança sinos de seu chapéu que causam dano aos inimigos. Quanto mais Chester joga seus sinos mais eles aumentam em número, aumentando seu dano. Suas Supers permitem que ele lance uma mola que atordoa os inimigos, outra joga uma bala explosiva, a terceira Chester come ua bala que cura sua vida, a quarta cria uma zona de estalinhos que deixam os inimigos lentos e causam dano, tornando-o, assim, uma ameaça ainda maior em combate.",
+        link: "https://pt.wikipedia.org/wiki.org/Brawl Stars#",
+        tags: "Palhaço palhaço PALHAÇO Lendário lendário Lendario lendario LENDÁRIO LENDARIO",
+        estatisticas: "Estatísticas: Vida = 7.400; Dano = 1.280; Velocidade = 770. Com acessório (Jujubas) = 1.020.",
+        modosDeJogo: "Melhores modos de jogo = Zona Estratégica e Fute-Brawl.",
+        dicasEstrategias: "Dicas e Estratégias: A propagação do ataque do Chester serve como indicador de quantos projéteis o próximo ataque vai disparar. O uso do Poder de estrela Batem os Sinos pode ser uma boa para lidar com adversários tanques, com bastante vida. Utilize a sequência de ataques de Chester para causar danos variados em diferentes situações. No início do jogo, dispare um sino para preparar os ataques mais poderosos com dois, três e quatro sinos."
+      },
+
+{
+  titulo: "Edgar",
+  imagem: "Imagem Edgar.png",
+  descricao: "Edgar é um brawler ágil e resistente, perfeito para jogadores que gostam de um estilo de jogo mais agressivo. Seu ataque básico consiste em uma série de socos rápidos, enquanto seu Super o impulsiona para cima, permitindo que ele pule barreiras e surpreenda seus adversários.  Os socos do Edgar recuperam sua vida conforme eles acertam o inimigo, tornando-o ainda mais perigoso em combate.",
+  link: "https://pt.wikipedia.org/wiki.org/Brawl Stars#",
+  tags: "emo Edgar EMO tóxico TÓXICO TOXICO toxico Tóxico Toxico melhorBrawler",
+  estatisticas: "Estatísticas: Vida = 6.600; Dano = 1.080; Velocidade = 820 (3.500 com super e 1.020 após usar o super, por alguns segundos).",
+  modosDeJogo: "Melhores modos de jogo: Combate Solitário, Combate Duplo, Roubo e Pique-Gema.",
+  dicasEstrategias: "Dicas e Estratégias: O Edgar possui uma habilidade após utilizar seu super, em que ele ganha uma velocidade de movimentação maior. Isso possibilita utiliza-lo para pegar gemas no modo Pique-Gema e parafusos no Encurralado, recuando rapidamente. Utilizando seu Acessório, ele pode carregar seu super mais rapidamente, para fazer uma fuga ou emboscar brawlers com vida baixa. Ele pode driblar a bola bem no Fute-Brawl, já que pode usar seu Super para pular para a bola e agarrá-la, semelhante ao que o El Primo e Darryl fazem. Se estiver jogando contra um Frank, é recomendado atacar e usar a velocidade de Edgar a seu favor. Cada vez que o Frank ataca, há um atraso que Edgar pode mover atrás dele e causar dano sem receber dano. Tente ficar longe de brawlers com alto dano de explosão, como Surge, Shelly, Bull ou Jacky, pois eles tendem abater o Edgar em 2 ou 3 acertos caso esteja com pouca vida. Com seu Acessório “Alçar Voo”, Edgar pode carregar rapidamente seu Super para fazer uma fuga rápida ou emboscar um alvo com pouca vida. Também pode ser usado para obter um carregamento rápido da habilidade quando eles são mais necessários, como no início de uma partida, para avançar na batalha."
+
+}
+
+
+
+    ];
+
